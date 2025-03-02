31cd7ec5193319600866bd8ffc9441d2c33d38ef
Rob Liedka <robertliedka@gmail.com>
2025-03-02 15:09:36 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world!");
+
+    get_user_input_number();
+}
+
+
+
+fn get_user_input_number() {
+    use std::io::{stdin,stdout,Write};
+    let mut s=String::new();
+    print!("Please enter some text: ");
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n')=s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r')=s.chars().next_back() {
+        s.pop();
+    }
+    println!("You typed: {}",s);
+}
